19d8c935f008a9ed62881b7614b0795b133ab391
IT21171338_T.M.T.C.Tennakoon <99181984+IT21171338@users.noreply.github.com>
2022-05-08 11:45:56 +0530

    Create main.cpp
    Uploaded

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include "Jobportal.h"
+
+using namespace std;
+
+int main(){
+  recruiter r1;
+  recruiter r2;
+  recruiter r3;
+
+  administrator admin1;
+  administrator admin2;
+  administrator admin3;
+
+  application ap1;
+  application ap2;
+  application ap3;
+  
+  return 0;
+}
